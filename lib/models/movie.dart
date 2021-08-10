9bd0811e5196ec3479efbf7dc69ae0ea7d295aed
bashiryousufy <bashiryousufy@gmail.com>
2021-08-10 17:09:41 +0800

    added movie widgets and displayed widgets on to UI

---
+++ lib/models/movie.dart
@@ -0,0 +1,44 @@
+//package
+import 'package:get_it/get_it.dart';
+
+//Model
+import '../models/app_config.dart';
+
+class Movie {
+  final String name;
+  final String language;
+  final bool isAdult;
+  final String description;
+  final String posterPath;
+  final String backdropPath;
+  final num rating;
+  final String releaseDate;
+
+  Movie(
+      {required this.name,
+      required this.language,
+      required this.isAdult,
+      required this.description,
+      required this.posterPath,
+      required this.backdropPath,
+      required this.rating,
+      required this.releaseDate});
+
+  factory Movie.fromJson(Map<String, dynamic> _json) {
+    return Movie(
+      name: _json['title'],
+      language: _json['orginal_language'],
+      isAdult: _json['adult'],
+      description: _json['overview'],
+      posterPath: _json['poster_path'],
+      backdropPath: _json['backdrop_path'],
+      rating: _json['voting_average'],
+      releaseDate: _json['release_date'],
+    );
+  }
+
+  String posterURL() {
+    final AppConfig _appconfig = GetIt.instance.get<AppConfig>();
+    return '${_appconfig.BASE_IMAGE_API_URL}${this.posterPath}';
+  }
+}
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,22 +1,31 @@
-//Packages
 import 'dart:ui';
-
+//Packages
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MainPage extends ConsumerWidget {
-  double _deviceHeight = 0;
-  double _deviceWidth = 0;
+//Widgets
+import '../widgets/movie_tile.dart';
 
+//Models
+import '../models/search_category.dart';
+import '../models/movie.dart';
+
+class MainPage extends ConsumerWidget {
+  late double _deviceHeight;
+  late double _deviceWidth;
+  late TextEditingController _searchTextFieldController;
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+
+    _searchTextFieldController = TextEditingController();
     return _buildUI();
   }
 
   Widget _buildUI() {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.black,
       body: Container(
         height: _deviceHeight,
@@ -25,6 +34,7 @@ class MainPage extends ConsumerWidget {
           alignment: Alignment.center,
           children: [
             _backgroundWidget(),
+            _forgroundWidget(),
           ],
         ),
       ),
@@ -52,5 +62,162 @@ class MainPage extends ConsumerWidget {
         ),
       ),
     );
+  }
+
+  Widget _forgroundWidget() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, _deviceHeight * 0.02, 0, 0),
+      width: _deviceWidth * 0.88,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _topBarWidget(),
+          Container(
+            height: _deviceHeight * 0.83,
+            padding: EdgeInsets.symmetric(vertical: _deviceHeight * 0.01),
+            child: _moviesListViewWidget(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return Container(
+      height: _deviceHeight * 0.08,
+      decoration: BoxDecoration(
+        color: Colors.black54,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _searchFieldWidget(),
+          _categorySelectionWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _searchFieldWidget() {
+    final _border = InputBorder.none;
+    return Container(
+      width: _deviceWidth * 0.50,
+      height: _deviceHeight * 0.05,
+      child: TextField(
+        controller: _searchTextFieldController,
+        onSubmitted: (_input) {
+          //TODO: Handle sumbit search
+        },
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        decoration: InputDecoration(
+          focusedBorder: _border,
+          border: _border,
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.white24,
+          ),
+          hintStyle: TextStyle(color: Colors.white54),
+          filled: false,
+          fillColor: Colors.white24,
+          hintText: 'Search....',
+        ),
+      ),
+    );
+  }
+
+  Widget _categorySelectionWidget() {
+    return DropdownButton(
+      dropdownColor: Colors.black38,
+      value: SearchCategory.popular,
+      icon: Icon(
+        Icons.menu,
+        color: Colors.white24,
+      ),
+      underline: Container(
+        height: 1,
+        color: Colors.white,
+      ),
+      onChanged: (_value) {
+        //TODO: on Change
+      },
+      items: [
+        DropdownMenuItem(
+          child: Text(
+            SearchCategory.popular,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          value: SearchCategory.popular,
+        ),
+        DropdownMenuItem(
+          child: Text(
+            SearchCategory.upcoming,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          value: SearchCategory.upcoming,
+        ),
+        DropdownMenuItem(
+          child: Text(
+            SearchCategory.none,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          value: SearchCategory.none,
+        ),
+      ],
+    );
+  }
+
+  Widget _moviesListViewWidget() {
+    final List<Movie> _movies = [];
+
+    for (var i = 0; i < 20; i++) {
+      _movies.add(Movie(
+          name: 'Mortal Combat',
+          language: 'EN',
+          isAdult: false,
+          description: 'Nice movie',
+          posterPath:
+              "https://m.media-amazon.com/images/M/MV5BY2ZlNWIxODMtN2YwZi00ZjNmLWIyN2UtZTFkYmZkNDQyNTAyXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_.jpg",
+          backdropPath:
+              'https://m.media-amazon.com/images/M/MV5BY2ZlNWIxODMtN2YwZi00ZjNmLWIyN2UtZTFkYmZkNDQyNTAyXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_.jpg',
+          rating: 7.8,
+          releaseDate: '2021-04-07'));
+    }
+    if (_movies.length != 0) {
+      return ListView.builder(
+          itemCount: _movies.length,
+          itemBuilder: (BuildContext _context, int _count) {
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: _deviceHeight * 0.01, horizontal: 0),
+              child: GestureDetector(
+                onTap: () {},
+                child: MovieTile(
+                  movie: _movies[_count],
+                  height: _deviceHeight * 0.20,
+                  width: _deviceWidth * 0.85,
+                ),
+              ),
+            );
+          });
+    } else {
+      return Center(
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.white,
+        ),
+      );
+    }
   }
 }
